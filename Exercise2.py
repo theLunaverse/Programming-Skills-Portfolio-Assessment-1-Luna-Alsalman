f641dad4b21ba5c8e72dcc3391920606f198ba6d
Luna Alsalman <manahel1506@gmail.com>
2024-10-26 13:15:19 +0400

    Basic addition exercise
    this code:
    - Assigns integer values to variables x and y
    - Adds x and y, storing the result in variable z
    - Prints the value of z, displaying the sum of x and y

---
+++ Exercise2.py
@@ -0,0 +1,8 @@
+#Luna Alsalman 588512
+#exercise 2
+
+x = 8  #assigns the value 8 to the variable x
+y = 10  #assigns the value 10 to the variable y
+z = x + y  #adds the values of x and y, and stores the result in z
+
+print(z)  #outputs the value of z, which is the sum of x and y
