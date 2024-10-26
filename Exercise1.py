95224f089bfad5f98fdb7c483f42974ddab772d5
Luna Alsalman <manahel1506@gmail.com>
2024-10-26 13:08:13 +0400

    String concatenation exercise
    this exercise:
    - Assigns individual words to variables `word1`, `word2`, and `word3`
    - Uses string concatenation to combine these words into the phrase "Coding is Cool"
    - Prints the concatenated phrase to the console

---
+++ Exercise1.py
@@ -0,0 +1,9 @@
+#Luna Alsalman 588512
+#exercise 1
+
+word1 = 'Coding '  #assigns the string 'coding' to the variable word1
+word2 = 'is ' #assigns the string 'is' to the variable word2
+word3 = 'Cool' #assigns the string 'cool' to the variable word3
+
+#use string concatenation to combine the variables and print the phrase
+print(word1 + word2 + word3)
